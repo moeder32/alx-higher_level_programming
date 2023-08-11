439e7eb0dbacf52385966ac12d34bd6f6e2a543f
moeder32 <kgomotso.mangoegape@yahoo.co.za>
2023-08-11 09:43:13 -0700

    tasks0 to tasks5

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
+
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
+    """To write a program that print the number of and list of arguments."""
     import sys
 
     count = len(sys.argv) - 1
@@ -11,3 +13,4 @@ if __name__ == "__main__":
         print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
